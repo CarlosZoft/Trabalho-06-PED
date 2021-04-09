40a83736e141c841f26e2e713c83a02526eb340e
hericklima22 <celebrogamer@gmail.com>
2021-04-09 17:29:51 -0300

    add o arquivo no sources

---
+++ ProjectVivado/driver_bcd_7seg/driver_bcd_7seg.srcs/sources_1/new/MUX_Quad_2_entradas.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09.04.2021 17:28:21
+-- Design Name: 
+-- Module Name: MUX_Quad_2_entradas - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity MUX_Quad_2_entradas is
+--  Port ( );
+end MUX_Quad_2_entradas;
+
+architecture Behavioral of MUX_Quad_2_entradas is
+
+begin
+
+
+end Behavioral;
